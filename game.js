643c53e0f1a014cce066f47da5ba56052e15e76e
KR_32 alias s1mpla <106810395+korki32@users.noreply.github.com>
2024-07-06 17:24:07 +0200

    Update game.js

--- game.js
+++ game.js
@@ -1,31 +1,25 @@
-const phrases = [
-    "Én még sosem ettem sushi-t.",
-    "Én még sosem voltam külföldön.",
-    "Én még sosem láttam a tengert.",
-    "Én még sosem ugráltam ejtőernyővel.",
-    "Én még sosem ettem egy egész tortát egyedül.",
-    "Én még sosem voltam éjszakai túrán.",
-    // Add more phrases here
-];
-
 let usedPhrases = JSON.parse(sessionStorage.getItem('usedPhrases')) || [];
 let cardCount = parseInt(sessionStorage.getItem('cardCount')) || 0;
 let players = JSON.parse(sessionStorage.getItem('players')) || [];
 let currentPlayerIndex = parseInt(sessionStorage.getItem('currentPlayerIndex')) || 0;
+let phrases = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('total-cards').textContent = phrases.length;
-    document.getElementById('card-count').textContent = cardCount;
-    updateCurrentPlayer();
-    document.getElementById('card').addEventListener('touchstart', function() {
-        flipCard();
-    });
+    fetch('phrases.json')
+        .then(response => response.json())
+        .then(data => {
+            phrases = data;
+            document.getElementById('total-cards').textContent = phrases.length;
+            document.getElementById('card-count').textContent = cardCount;
+            updateCurrentPlayer();
+        })
+        .catch(error => console.error('Error loading phrases:', error));
 });
 
 function flipCard() {
     const card = document.getElementById('card');
     card.classList.toggle('flipped');
-    
+
     if (card.classList.contains('flipped')) {
         const remainingPhrases = phrases.filter(phrase => !usedPhrases.includes(phrase));
         if (remainingPhrases.length === 0) {
