98bda319abba981b064b23d635fdc2adca0f4e4b
KR_32 alias s1mpla <106810395+korki32@users.noreply.github.com>
2024-07-10 15:41:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -31,14 +31,4 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('card').addEventListener('touchstart', function() {
         flipCard();
     });
-
-    function flipCard() {
-        const card = document.getElementById('card');
-        card.classList.toggle('flipped');
-
-        if (card.classList.contains('flipped')) {
-            const randomIndex = Math.floor(Math.random() * phrases.length);
-            document.getElementById('card-text').textContent = phrases[randomIndex];
-        }
-    }
 });
