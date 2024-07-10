d0f31b2999725579d459f79035bca69e49138de4
KR_32 alias s1mpla <106810395+korki32@users.noreply.github.com>
2024-07-10 15:20:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ function flipCard() {
 document.getElementById('card').addEventListener('touchstart', function() {
     flipCard();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    sessionStorage.clear();
+
+    document.getElementById('player-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        const players = document.getElementById('players').value.split(',').map(name => name.trim());
+        const includeSpecialCards = document.getElementById('include-special-cards').checked;
+        sessionStorage.setItem('players', JSON.stringify(players));
+        sessionStorage.setItem('includeSpecialCards', includeSpecialCards);
+        window.location.href = 'game.html';
+    });
+
+    const animateButton = function (e) {
+        e.preventDefault();
+        e.target.classList.remove('animate');
+        e.target.classList.add('animate');
+        setTimeout(function () {
+            e.target.classList.remove('animate');
+        }, 700);
+    };
+
+    const bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+    for (let i = 0; i < bubblyButtons.length; i++) {
+        bubblyButtons[i].addEventListener('click', animateButton, false);
+    }
+});
